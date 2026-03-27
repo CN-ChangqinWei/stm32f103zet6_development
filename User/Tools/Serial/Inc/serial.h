0b1edb37a13111869d49737b249ef190f4946f04
CN-ChangqinWei <2296195685@qq.com>
2026-03-27 23:33:56 +0800

    add:完成serial函数实现

--- User/Tools/Serial/Inc/serial.h
+++ User/Tools/Serial/Inc/serial.h
@@ -2,4 +2,32 @@
 #define _SERIAL_H
 #include "stm32f1xx_hal.h"
 
+typedef struct{
+    UART_HandleTypeDef* uart;
+    uint8_t * recvBuf;//接收数组的指针
+    uint32_t  recvLen;//接收数组的长度
+    uint8_t * sendBuf;//发送数组的指针
+    uint32_t  sendLen;//发送数组的长度
+    #ifdef HAL_DMA_MODULE_ENABLED
+    DMA_HandleTypeDef* dma;//如果dma不为0那么recvbuf和sendbuf
+    #endif
+
+} Serial ;
+
+Serial NewSerial(UART_HandleTypeDef* uart,
+    uint8_t * recvBuf,
+    uint32_t  recvLen,
+    uint8_t * sendBuf,
+    uint32_t  sendLen
+    #ifdef HAL_DMA_MODULE_ENABLED
+    ,DMA_HandleTypeDef* dma
+    #endif
+);
+
+uint8_t SerialSetRecvBuf(uint8_t* buf);
+uint8_t SerialSetSendBuf(uint8_t* buf);
+uint32_t SerialSend(uint32_t len);//发送指定字节 如果dma存在且实例中有搬运的地址与结构体设定相同，那么用dma搬运
+uint32_t SerialRecv();//接收字节到recvbuf,dma要求同send
+uint32_t SerialSendUseOtherBuf(uint8_t* buf,uint32_t len);//发送其它的buf如果buf和sendbuf相同那么使用SerialSend
+uint32_t SerialRecvUseOtherBuf(uint8_t* buf,uint32_t len);//接收数据到其它buf 如果buf相同和send同一种处置方式
 #endif
--- User/Tools/Serial/Src/serial.c
+++ User/Tools/Serial/Src/serial.c
@@ -1,1 +1,101 @@
 #include"serial.h"
+
+Serial NewSerial(UART_HandleTypeDef* uart,
+    uint8_t * recvBuf,
+    uint32_t  recvLen,
+    uint8_t * sendBuf,
+    uint32_t  sendLen
+    #ifdef HAL_DMA_MODULE_ENABLED
+    ,DMA_HandleTypeDef* dma
+    #endif
+){
+    Serial serial;
+    serial.uart = uart;
+    serial.recvBuf = recvBuf;
+    serial.recvLen = recvLen;
+    serial.sendBuf = sendBuf;
+    serial.sendLen = sendLen;
+    #ifdef HAL_DMA_MODULE_ENABLED
+    serial.dma = dma;
+    #endif
+    return serial;
+}
+
+uint8_t SerialSetRecvBuf(Serial* serial, uint8_t* buf, uint32_t len){
+    if(serial == NULL || buf == NULL) return 1;
+    serial->recvBuf = buf;
+    serial->recvLen = len;
+    return 0;
+}
+
+uint8_t SerialSetSendBuf(Serial* serial, uint8_t* buf, uint32_t len){
+    if(serial == NULL || buf == NULL) return 1;
+    serial->sendBuf = buf;
+    serial->sendLen = len;
+    return 0;
+}
+
+uint32_t SerialSend(Serial* serial, uint32_t len){
+    if(serial == NULL || serial->sendBuf == NULL || serial->uart == NULL) return 0;
+    if(len > serial->sendLen) len = serial->sendLen;
+    
+    #ifdef HAL_DMA_MODULE_ENABLED
+    if(serial->dma != NULL) {
+        HAL_UART_Transmit_DMA(serial->uart, serial->sendBuf, len);
+    } else
+    #endif
+    {
+        HAL_UART_Transmit(serial->uart, serial->sendBuf, len, HAL_MAX_DELAY);
+    }
+    return len;
+}
+
+uint32_t SerialRecv(Serial* serial){
+    if(serial == NULL || serial->recvBuf == NULL || serial->uart == NULL) return 0;
+    
+    #ifdef HAL_DMA_MODULE_ENABLED
+    if(serial->dma != NULL) {
+        HAL_UART_Receive_DMA(serial->uart, serial->recvBuf, serial->recvLen);
+    } else
+    #endif
+    {
+        HAL_UART_Receive(serial->uart, serial->recvBuf, serial->recvLen, HAL_MAX_DELAY);
+    }
+    return serial->recvLen;
+}
+
+uint32_t SerialSendUseOtherBuf(Serial* serial, uint8_t* buf, uint32_t len){
+    if(serial == NULL || buf == NULL || serial->uart == NULL) return 0;
+    
+    if(buf == serial->sendBuf) {
+        return SerialSend(serial, len);
+    }
+    
+    #ifdef HAL_DMA_MODULE_ENABLED
+    if(serial->dma != NULL) {
+        HAL_UART_Transmit_DMA(serial->uart, buf, len);
+    } else
+    #endif
+    {
+        HAL_UART_Transmit(serial->uart, buf, len, HAL_MAX_DELAY);
+    }
+    return len;
+}
+
+uint32_t SerialRecvUseOtherBuf(Serial* serial, uint8_t* buf, uint32_t len){
+    if(serial == NULL || buf == NULL || serial->uart == NULL) return 0;
+    
+    if(buf == serial->recvBuf) {
+        return SerialRecv(serial);
+    }
+    
+    #ifdef HAL_DMA_MODULE_ENABLED
+    if(serial->dma != NULL) {
+        HAL_UART_Receive_DMA(serial->uart, buf, len);
+    } else
+    #endif
+    {
+        HAL_UART_Receive(serial->uart, buf, len, HAL_MAX_DELAY);
+    }
+    return len;
+}
