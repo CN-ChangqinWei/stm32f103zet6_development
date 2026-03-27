f930cd1a4d2432f048caf283f667b27d934adc28
CN-ChangqinWei <2296195685@qq.com>
2026-03-27 22:49:06 +0800

    add:添加serial模块以及cmake

---
+++ User/Tools/Serial/Inc/serial.h
@@ -0,0 +1,5 @@
+#ifndef _SERIAL_H
+#define _SERIAL_H
+#include "stm32f1xx_hal.h"
+
+#endif
---
+++ User/Tools/Serial/Inc/serial_test.h
@@ -0,0 +1,6 @@
+#ifndef _SERIAL_TEST_H
+#define _SERIAL_TEST_H
+#include "stm32f1xx_hal.h"
+#include "serial.h"
+
+#endif
